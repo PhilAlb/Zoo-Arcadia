d5acf2d784a213ab869bae51766a6d0cd0f12c84
PhilAlb <alabau.philippe@gmail.com>
2024-07-22 22:52:31 +0200

    Corrected the launch_scripts

--- launch_scripts.sh
+++ launch_scripts.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Function to clean up background jobs
+cleanup() {
+    echo "Cleaning up..."
+    exit
+}
+
+# Trap SIGINT and SIGTERM
+trap cleanup SIGINT SIGTERM
+
 ./front_run.sh &
+./back_run.sh
 
-./back_run.sh &
-
+# Wait for all background processes to finish
 wait
