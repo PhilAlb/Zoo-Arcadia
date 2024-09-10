60b1891ada753bfd937eb9f91e687d02fe2a64da
PhilAlb <alabau.philippe@gmail.com>
2024-09-10 15:36:21 +0200

    corrected the server configuration

--- Arcadia-front/environment.prod.ts
+++ Arcadia-front/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://arcadia-coop-back.onrender.com', // The base URL of the .NET backend
+  apiUrl: "https://arcadia-back.onrender.com", // The base URL of the .NET backend
 };
